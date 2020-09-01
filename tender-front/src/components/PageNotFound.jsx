66aa1058ebdfe6d647e002266c475456bc964158
fcesc-code <bpfran@gmail.com>
2020-09-01 09:40:12 +0200

    hotfix component file type and imports

--- tender-front/src/components/PageNotFound.jsx
+++ tender-front/src/components/PageNotFound.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ROUTES from './../routes/ROUTES';
+import ROUTES from '../routes/ROUTES';
 
 function PageNotFound() {
   return (
