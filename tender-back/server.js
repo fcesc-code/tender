4c8c33a30bb48aa9da9261b1c408ef19fa01ead5
fcesc-code <bpfran@gmail.com>
2020-09-01 09:18:01 +0200

    update backend settings

---
+++ tender-back/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const debug = require('debug')('server:server.js');
+const chalk = require('chalk');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3010;
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+const testRoutes = require('./src/routes/testRoutes')();
+
+server.use('/api/test', testRoutes);
+
+server.listen(port, ()=>{debug(`Listening on port, ${chalk.blueBright(port)}`)});
