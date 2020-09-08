50fcf84051d8ba07903e41f9590183a21807fc81
fcesc-code <bpfran@gmail.com>
2020-09-08 11:43:02 +0200

    hotfix action types

--- tender-back/server.js
+++ tender-back/server.js
@@ -17,7 +17,7 @@ server.use(bodyParser.json());
 
 // server.use(cors());
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'localhost:3000');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   next();
 });
 
