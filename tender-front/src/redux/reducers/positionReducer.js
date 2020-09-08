0d916f668d938b9c735d3ca884ed27ca71f0aa28
fcesc-code <bpfran@gmail.com>
2020-09-08 11:53:50 +0200

    hotfix quotation action types

--- tender-front/src/redux/reducers/positionReducer.js
+++ tender-front/src/redux/reducers/positionReducer.js
@@ -3,17 +3,17 @@ import REDUCERS_INITIAL_STATE from './REDUCERS_INITIAL_STATE';
 
 export default function positionReducer(state = REDUCERS_INITIAL_STATE.position, action = ACTION_TYPES.DEFAULT) {
   switch (action.type) {
-    case ACTION_TYPES.POSITION.CREATE_POSITION:
+    case ACTION_TYPES.QUOTATION.CREATE_QUOTATION:
       return action.payload;
-    case ACTION_TYPES.POSITION.UPDATE_POSITION:
+    case ACTION_TYPES.QUOTATION.UPDATE_QUOTATION:
       return action.payload;
-    case ACTION_TYPES.POSITION.DELETE_POSITION:
+    case ACTION_TYPES.QUOTATION.DELETE_QUOTATION:
       return {};
-    case ACTION_TYPES.POSITION.LOAD_POSITION:
+    case ACTION_TYPES.QUOTATION.LOAD_QUOTATION:
       return action.payload;
-    case ACTION_TYPES.POSITION.ADD_DISCOUNT_POSITION:
+    case ACTION_TYPES.QUOTATION.ADD_DISCOUNT_QUOTATION:
       return action.payload;
-    case ACTION_TYPES.POSITION.UPDATE_DISCOUNT_POSITION:
+    case ACTION_TYPES.QUOTATION.UPDATE_DISCOUNT_QUOTATION:
       return action.payload;
     default:
       return state;
