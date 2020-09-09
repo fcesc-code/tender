1af78035f8f5d052799ca91795183c9df0e91861
fcesc-code <bpfran@gmail.com>
2020-09-09 18:09:04 +0200

    update portfolio info

--- tender-front/src/components/portfolio/PortfolioInfo.jsx
+++ tender-front/src/components/portfolio/PortfolioInfo.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import portfolio_mock from '../../mockdata-front/portfolio_mock';
+import { connect } from 'react-redux';
 import TuneIcon from '@material-ui/icons/Tune';
 import SortByAlphaIcon from '@material-ui/icons/SortByAlpha';
 import './portfolioInfo.sass';
 
-function PortfolioInfo() {
-  const projects = portfolio_mock;
+function PortfolioInfo({ projects }) {
+
+  function displayInfo(){
+  return ( projects !== undefined && projects.length !== 0 ) ? 
+    (
+      <p>
+        <span>You have {projects.length} projects in your portfolio:</span>
+        <span> {projects.filter(project => project.status === 'ongoing').length} ongoing,</span>
+        <span> {projects.filter(project => project.status === 'planned').length} planned,</span>
+        <span> {projects.filter(project => project.status === 'closed').length} closed.</span>
+      </p>
+    ) : '';
+  }
+
   return (
     <div className="portfolioInfo__container">
       <div className='info__main'>
         <h2>YOUR PROJECTS</h2>
-        <p>
-          <span>You have {projects.length} projects in your portfolio:</span>
-          <span> {projects.filter(project => project.status === 'ongoing').length} ongoing,</span>
-          <span> {projects.filter(project => project.status === 'planned').length} planned,</span>
-          <span> {projects.filter(project => project.status === 'closed').length} closed.</span>
-        </p>
+        {displayInfo()}
       </div>
       <div className='info__icons'>
         <div className='icon'>
@@ -29,4 +36,11 @@ function PortfolioInfo() {
   );
 }
 
-export default PortfolioInfo;
+function mapStateToProps(state){
+  return ({
+    projects: state.portfolio.data,
+    userId: state.user.uid
+  });
+}
+
+export default connect(mapStateToProps)(PortfolioInfo);
