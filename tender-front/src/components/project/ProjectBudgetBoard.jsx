00eb96c74de3cea3deb9d6d6196153dd96825415
fcesc-code <bpfran@gmail.com>
2020-09-11 12:58:13 +0200

    feature format currency

--- tender-front/src/components/project/ProjectBudgetBoard.jsx
+++ tender-front/src/components/project/ProjectBudgetBoard.jsx
@@ -4,7 +4,6 @@ import './projectBudgetBoard.sass';
 import Spinner from '../common/Spinner';
 
 function ProjectBudgetBoard( { budget } ) {
-  for (let p in budget){ console.log('prop ', p)};
 
   return (
     ( Object.keys(budget).length === 0 ) ? (<Spinner/>) : (
@@ -31,7 +30,35 @@ function ProjectBudgetBoard( { budget } ) {
 }
 
 function mapStateToProps( state, { view } ){
-  const selectedBudget = (Object.keys(state.project.data).length === 0) ? '' : ( view === '' ) ? state.project.data[0] : state.project.data.filter(item=>item._id === view)[0];
+  const selectedBudget = (Object.keys(state.project.data).length === 0) ? '' : ( view === '' ) ? { ...state.project.data[0] } : { ...state.project.data.filter(item=>item._id === view)[0] };
+ 
+  function formatSelectedBudget(budget){
+    console.log('ENTERING FORMATTING FUNCTION WITH ', budget, budget.data.entries);
+    const currency = budget.currency;
+    for (let entry of budget.data.entries){
+      entry[4] = formatIntToCurrency(entry[4], currency);
+      entry[5] = formatIntToCurrency(entry[5], currency);
+    }
+    console.log('EXITING FORMATTING FUNCTION WITH ', budget);
+  }
+
+  function formatIntToCurrency(currentNumber, currency){
+    const newNumber = (Number(currentNumber) / 100).toFixed(2);
+    let newCurrency;
+    switch(currency){
+      case 'eur':
+        newCurrency = '€';
+        break;
+      default:
+        newCurrency = currency;
+    }
+    const newValue = `${newNumber} €`;
+    return newValue;
+  }
+
+  const formatedBudget =  (selectedBudget === '' && view === '') ? '' : formatSelectedBudget(selectedBudget);
+  console.log('AQUI FORMAT', formatedBudget);
+
   return {
     budget: selectedBudget
   }
