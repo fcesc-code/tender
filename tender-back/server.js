bbddc34adb31de6b88e773cbc423227285463d5b
fcesc-code <bpfran@gmail.com>
2021-08-12 11:36:24 +0200

    updated backend server for tender app

--- tender-back/server.js
+++ tender-back/server.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const debug = require('debug')('server:server.js');
-const chalk = require('chalk');
-const bodyParser = require('body-parser');
-const DATABASE_CONFIG = require('./database/DATABASE_CONFIG');
-const jwt = require('express-jwt');
-const jwks = require('jwks-rsa');
-const jwtAuthz = require('express-jwt-authz');
-const JWT_CONFIG = require('./config/JWT_CONFIG');
-const cors = require('cors');
+const express = require("express");
+const debug = require("debug")("server:server.js");
+const chalk = require("chalk");
+const bodyParser = require("body-parser");
+const DATABASE_CONFIG = require("./database/DATABASE_CONFIG");
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+const jwtAuthz = require("express-jwt-authz");
+const JWT_CONFIG = require("./config/JWT_CONFIG");
+const cors = require("cors");
+
+console.log("TEST", JWT_CONFIG);
 
 const PORT = process.env.PORT || 3010;
 const server = express();
@@ -19,34 +21,44 @@ server.use(cors());
 
 const jwtCheck = jwt({
   secret: jwks.expressJwtSecret({
-      cache: JWT_CONFIG.cache,
-      rateLimit: JWT_CONFIG.rateLimit,
-      jwksRequestsPerMinute: JWT_CONFIG.jwksRequestsPerMinute,
-      jwksUri: JWT_CONFIG.jwksUri
+    cache: JWT_CONFIG.cache,
+    rateLimit: JWT_CONFIG.rateLimit,
+    jwksRequestsPerMinute: JWT_CONFIG.jwksRequestsPerMinute,
+    jwksUri: JWT_CONFIG.jwksUri,
   }),
   audience: JWT_CONFIG.audience,
   issuer: JWT_CONFIG.issuer,
-  algorithms: JWT_CONFIG.algorithms
+  algorithms: JWT_CONFIG.algorithms,
 });
 
-server.get('/authorized', function (req, res) {
-  res.send('Secured Resource');
+server.get("/authorized", function (req, res) {
+  res.send("Secured Resource");
 });
 
-const projectRoutes = require('./src/routes/projectRoutes')(DATABASE_CONFIG.projectsCollection);
+const projectRoutes = require("./src/routes/projectRoutes")(
+  DATABASE_CONFIG.projectsCollection
+);
 
-server.use('/api/projects', jwtCheck, projectRoutes);
+server.use("/api/projects", jwtCheck, projectRoutes);
 
-const budgetRoutes = require('./src/routes/budgetRoutes')(DATABASE_CONFIG.budgetsCollection);
+const budgetRoutes = require("./src/routes/budgetRoutes")(
+  DATABASE_CONFIG.budgetsCollection
+);
 
-server.use('/api/budgets', jwtCheck, budgetRoutes);
+server.use("/api/budgets", jwtCheck, budgetRoutes);
 
-const quotationRoutes = require('./src/routes/quotationRoutes')(DATABASE_CONFIG.quotationsCollection);
+const quotationRoutes = require("./src/routes/quotationRoutes")(
+  DATABASE_CONFIG.quotationsCollection
+);
 
-server.use('/api/quotations', jwtCheck, quotationRoutes);
+server.use("/api/quotations", jwtCheck, quotationRoutes);
 
-const userRoutes = require('./src/routes/userRoutes')(DATABASE_CONFIG.usersCollection);
+const userRoutes = require("./src/routes/userRoutes")(
+  DATABASE_CONFIG.usersCollection
+);
 
-server.use('/api/users', jwtCheck, userRoutes);
+server.use("/api/users", jwtCheck, userRoutes);
 
-server.listen(PORT, ()=>{ console.log(`Server listening on port ${chalk.blueBright(PORT)}`)});
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${chalk.blueBright(PORT)}`);
+});
